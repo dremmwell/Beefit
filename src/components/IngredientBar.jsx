1383f567bcf8efc36fc7962972897440f980b3dc
dremmwell <dremmwell4>
2023-09-07 22:31:01 +0100

    Ingredient tab layout

---
+++ src/components/IngredientBar.jsx
@@ -0,0 +1,12 @@
+import ingredientBar from '../styles/IngredientBar.module.css';
+
+function IngredientBar(){
+
+    return(
+        <div className={ingredientBar.container}>
+            Ingredients Bar
+        </div>
+    )
+}
+
+export default IngredientBar
--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,10 +1,76 @@
 import ingredients from '../styles/Ingredients.module.css';
+import IngredientBar from './IngredientBar';
 
 function Ingredients(){
 
     return(
         <div className={ingredients.container}>
-            Ingredients Tab
+            <div className={ingredients.title}>My Ingredients</div>
+            <div className={ingredients.legend}>
+                <h1>Calories</h1>
+                <h1>Proteines</h1>
+                <h1>Carbs</h1>
+                <h1>Fats</h1>
+            </div>
+            <div className={ingredients.table}>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+                <IngredientBar/>
+            </div>
+            <div className={ingredients.searchBar}>Search</div>
+            <div className={ingredients.addIngredient}>+ Add a new ingredient</div>
         </div>
     )
 }
