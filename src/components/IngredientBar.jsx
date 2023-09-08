b3ee54a058c517ed9a557ff94abff4bddad8f95b
dremmwell <dremmwell4>
2023-09-08 19:41:45 +0100

    ingredient bars and tab style

--- src/components/IngredientBar.jsx
+++ src/components/IngredientBar.jsx
@@ -1,12 +1,27 @@
 import ingredientBar from '../styles/IngredientBar.module.css';
+import PropTypes from 'prop-types';
 
-function IngredientBar(){
+function IngredientBar({ingredient}){
 
     return(
-        <div className={ingredientBar.container}>
-            Ingredients Bar
+        <div className={ingredientBar.bar}>
+            <div className={ingredientBar.title}>
+                <div className={ingredientBar.name}>{ingredient.name}</div>
+                <div className={ingredientBar.per}>per 100g</div>
+            </div>
+            <div className={ingredientBar.properties}>
+                <h1>Calories</h1>
+                <h1>Proteines</h1>
+                <h1>Carbs</h1>
+                <h1>Fats</h1>
+            </div>
         </div>
     )
 }
 
-export default IngredientBar
+export default IngredientBar;
+
+IngredientBar.propTypes={
+    ingredient: PropTypes.object,
+    key: PropTypes.number,
+}
